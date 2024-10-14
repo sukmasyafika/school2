37ec25eedec3147b24ed3acdc504201d1f35b8a7
sukmasyafika <98916879+sukmasyafika@users.noreply.github.com>
2024-10-14 10:47:28 +0900

    Update berita.php

--- pages/berita.php
+++ pages/berita.php
@@ -2,127 +2,77 @@
 
 <?= $this->section('content'); ?>
 
-<!-- profil -->
-<section id="about" class="container mt-5 py-5">
-  <div class="mb-5 text-center">
-    <h2 class="display-4 pb-3 fw-semibold">Profil <span class="text-info">SMK N 2</span></h2>
-    <p class="fst-italic">SMKN 2 merupakan sekolah terbagus dan terluas yang ada di Papua. Ada 5 jurusan yang siap mempersiapkan Anda menjadi orang sukses.</p>
-  </div>
-  <div class="row justify-content-center">
-    <div class=" text-center mb-4">
-      <img src="<?= base_url('assets/img/about/ks-1.jpg'); ?>" alt="Kepala Sekolah" class="img-fluid rounded shadow-lg" width="250">
-      <h4 class="mt-3 fw-bold">Kepala Sekolah</h4>
-    </div>
-    <div class="col">
-      <div class="card shadow-sm">
-        <div class="card-body">
-          <p class="card-text">
-            SMKN 2 Bisnis dan Manajemen Jayapura merupakan salah satu SMK Pusat Keunggulan yang ada di Provinsi Papua, terletak di Jl. Raya Kotaraja Dalam, Abepura Papua. Sebagaimana sekolah menengah kejuruan lainnya, sekolah ini juga mengelola beberapa Program Keahlian di antaranya Manajemen Perkantoran dan Layanan Bisnis, Akuntansi dan Keuangan Lembaga, Pemasaran, Usaha Layanan Pariwisata, dan Teknik Jaringan Komputer dan Telekomunikasi. SMKN 2 Bisnis dan Manajemen didukung oleh tenaga GTK yang kompeten di bidangnya sehingga berkualitas dan menjadi salah satu SMK terbaik di Kota Jayapura.
-          </p>
-          <h2 class="card-title mt-4 mb-4 fw-medium">Visi</h2>
-          <p class="card-text">Sebagai Lembaga Diklat Kejuruan yang Berstandar Mutu dan Tetap Mengemban Potensi Tanah Papua Menuju Kemandirian.</p>
-          <h2 class="card-title mb-4 fw-medium">Misi</h2>
-          <ul class="list-unstyled">
-            <li class="d-flex align-items-start mb-2">
-              <span class="badge bg-info rounded-pill me-3">1</span>
-              <span>Menyelenggarakan PBM yang Bermutu, Berakhlak dan Bermoral.</span>
-            </li>
-            <li class="d-flex align-items-start mb-2">
-              <span class="badge bg-info rounded-pill me-3">2</span>
-              <span>Mengembangkan kegiatan Diklat yang Inovatif, Kreatif dan Produktif.</span>
-            </li>
-            <li class="d-flex align-items-start mb-2">
-              <span class="badge bg-info rounded-pill me-3">3</span>
-              <span>Mendorong partisipasi aktif dalam menjaga lingkungan.</span>
-            </li>
-            <li class="d-flex align-items-start mb-2">
-              <span class="badge bg-info rounded-pill me-3">4</span>
-              <span>Mengembangkan keterampilan dan kreativitas siswa.</span>
-            </li>
-            <li class="d-flex align-items-start mb-2">
-              <span class="badge bg-info rounded-pill me-3">5</span>
-              <span>Menyediakan layanan Diklat bermutu bagi Lembaga-Lembaga Dinas/Instansi, sesuai dengan bidang keahlian yang ada.</span>
-            </li>
-          </ul>
+<!-- artikel -->
+<main>
+  <section class="container py-5">
+    <h2 class="p-5 display-4 text-center fw-semibold">Berita</h2>
+    <div class="row">
+      <!-- Bagian Kiri -->
+      <div class="col-lg-6">
+        <h3 class="fw-bold mb-5 text-info">Berita Utama</h3>
+        <div class="row">
+          <div class="col-12 mb-5">
+            <article>
+              <img src="<?= base_url('assets/img/berita/ppdb.png'); ?>" class="img-fluid" alt="Top Story Image">
+              <h4 class="mt-3 fw-bold">Penerimaan Siswa Baru Tahun Ajaran 2024/2025</h4>
+              <p>SMK N 2 Jayapura membuka pendaftaran siswa baru untuk tahun ajaran 2024/2025. Segera daftarkan diri Anda melalui portal resmi sekolah.</p>
+              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+            </article>
+          </div>
+          <div class="col-12 col-md-6 mb-4">
+            <article>
+              <img src="<?= base_url('assets/img/berita/sains.jpg'); ?>" class="img-fluid" alt="Bottom Left Image 1">
+              <h4 class="mt-3 fw-bold">Kegiatan Lomba Sains Tingkat Provinsi</h4>
+              <p>Siswa SMK N 2 Jayapura berhasil meraih juara 1 dalam lomba sains tingkat provinsi yang diadakan di Kota Jayapura.</p>
+              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+            </article>
+          </div>
+          <div class="col-12 col-md-6 mb-4">
+            <article>
+              <img src="<?= base_url('assets/img/berita/b-2.jpg'); ?>" class="img-fluid" alt="Bottom Left Image 2">
+              <h4 class="mt-3 fw-bold">Workshop Teknologi Informasi untuk Guru</h4>
+              <p>Guru-guru SMK N 2 Jayapura mengikuti workshop tentang teknologi informasi untuk meningkatkan keterampilan dalam pengajaran.</p>
+              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+            </article>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</section>
-<!-- profil -->
 
-<!-- pendidikan -->
-<section id="tenaga" class="container py-5">
-  <div class="mb-5 text-center">
-    <h2 class="display-4 pb-3 fw-semibold">Tenaga Pendidikan <span class="text-info">Dan Kependudukan</span></h2>
-  </div>
-  <div class="row justify-content-center">
-    <div class="col-12 col-md-4 mb-4">
-      <div class="card shadow">
-        <img src="<?= base_url('assets/img/tenaga/tenaga1.jpg'); ?>" class="card-img-top" alt="Budi Santoso" height="250">
-        <div class="card-body text-center">
-          <h5 class="card-title">Budi Santoso</h5>
-          <p class="card-text">Jabatan: Guru Kelas<br>
-            Mata Pelajaran: Matematika</p>
-        </div>
+      <div class="col-lg-6">
+        <h3 class="fw-bold mb-5 text-info">Berita Terbaru</h3>
+        <article class="d-flex mb-4">
+          <div class="me-3">
+            <h4 class="fw-bold">Pembagian Rapor Semester Ganjil 2024/2025</h4>
+            <p class="text-small">Pembagian rapor semester ganjil akan dilaksanakan pada tanggal 20 Desember 2024 di ruang kelas masing-masing.</p>
+            <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+          </div>
+          <img src="<?= base_url('assets/img/berita/b-5.jpg'); ?>" class="img-fluid" alt="Right Image 1" style="width: 120px; height: auto;">
+        </article>
+        <hr class="border border-info border-1 opacity-50">
+        <article class="d-flex mb-4">
+          <div class="me-3">
+            <h4 class="fw-bold">Persiapan Ujian Praktik Kejuruan 2024</h4>
+            <p class="text-small">Ujian praktik kejuruan bagi kelas XII akan dilaksanakan mulai Januari 2024. Pastikan semua perlengkapan dan materi ujian telah disiapkan.</p>
+            <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+          </div>
+          <img src="<?= base_url('assets/img/berita/b-5.jpg'); ?>" class="img-fluid" alt="Right Image 2" style="width: 120px; height: auto;">
+        </article>
+        <hr class="border border-info border-1 opacity-50">
+        <article class="d-flex mb-4">
+          <div class="me-3">
+            <h4 class="fw-bold">Kegiatan Perkemahan Pramuka di SMK N 2 Jayapura</h4>
+            <p class="text-small">Pramuka SMK N 2 Jayapura akan mengadakan kegiatan perkemahan tahunan pada bulan Oktober 2024. Kegiatan ini diikuti oleh seluruh siswa kelas X dan XI.</p>
+            <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+          </div>
+          <img src="<?= base_url('assets/img/berita/b-5.jpg'); ?>" class="img-fluid" alt="Right Image 3" style="width: 120px; height: auto;">
+        </article>
+        <hr class="border border-info border-1 opacity-50">
       </div>
     </div>
-    <div class="col-12 col-md-4 mb-4">
-      <div class="card shadow">
-        <img src="<?= base_url('assets/img/tenaga/g-3.png'); ?>" class="card-img-top" alt="Budi Santoso" height="250">
-        <div class="card-body text-center">
-          <h5 class="card-title">Siti Nurjanah</h5>
-          <p class="card-text">Jabatan: Guru Bahasa Inggris<br>
-            Mata Pelajaran: Bahasa Inggris</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-12 col-md-4 mb-4">
-      <div class="card shadow">
-        <img src="<?= base_url('assets/img/tenaga/g-2.jpg'); ?>" class="card-img-top" alt="Budi Santoso" height="250">
-        <div class="card-body text-center">
-          <h5 class="card-title">Ahmad Taufik</h5>
-          <p class="card-text">Jabatan: Guru Sains<br>
-            Mata Pelajaran: Fisika</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- pendidikan -->
+  </section>
 
-<!-- fasitas -->
-<section id="fasilitas" class="container py-5">
-  <div class="mb-5 text-center">
-    <h2 class="display-4 pb-3 fw-semibold">Fasilitas <span class="text-info">Sekolah</span></h2>
-    <div class="row justify-content-center">
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card shadow">
-          <img src="<?= base_url('assets/img/about/kelas.jpg'); ?>" class="card-img-top" alt="Budi Santoso" height="250">
-          <div class="card-body text-center">
-            <h5 class="card-title">Ruang Kelas</h5>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card shadow">
-          <img src="<?= base_url('assets/img/about/lab.jpg'); ?>" class="card-img-top" alt="Budi Santoso" height="250">
-          <div class="card-body text-center">
-            <h5 class="card-title">Lab Tiap Jurusan</h5>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card shadow">
-          <img src="<?= base_url('assets/img/about/olahraga.jpg'); ?>" class="card-img-top" alt="Budi Santoso" height="250">
-          <div class="card-body text-center">
-            <h5 class="card-title">Lapangan olahraga</h5>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- fasitas -->
+</main>
+
+<!-- artikel -->
 
 <?= $this->endSection(); ?>
