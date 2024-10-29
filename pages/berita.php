14f560a1e2467920b421ddb0f8845a1d91a9ae90
Fanny <fanigiya@gmail.com>
2024-10-30 03:00:30 +0900

    Update berita.php

--- pages/berita.php
+++ pages/berita.php
@@ -13,46 +13,44 @@
         <div class="row">
           <div class="col-12 mb-5">
             <article>
-              <img src="<?= base_url('assets/img/berita/ppdb.png'); ?>" class="img-fluid" alt="Top Story Image">
-              <h4 class="mt-3 fw-bold">Penerimaan Siswa Baru Tahun Ajaran 2024/2025</h4>
-              <p>SMK N 2 Jayapura membuka pendaftaran siswa baru untuk tahun ajaran 2024/2025. Segera daftarkan diri Anda melalui portal resmi sekolah.</p>
-              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+              <!-- Carousel -->
+              <div id="newsCarousel" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="<?= base_url('assets/img/berita/ppdb.png'); ?>" class="d-block w-100 img-fluid" alt="Top Story Image 1">
+                    <h4 class="mt-3 fw-bold">Penerimaan Siswa Baru Tahun Ajaran 2024/2025</h4>
+                    <p>SMK N 2 Jayapura membuka pendaftaran siswa baru untuk tahun ajaran 2024/2025. Segera daftarkan diri Anda melalui portal resmi sekolah.</p>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="<?= base_url('assets/img/berita/ppd.jpg'); ?>" class="d-block w-100 img-fluid" alt="Top Story Image 2">
+                    <h4 class="mt-3 fw-bold">Pengumuman Pendaftaran Tahun ajaran 2024/2025</h4>
+                    <p>PPDB mulai dari persiapan (pra pendaftaran), pengumuman pendaftaran, pendaftaran dan penyerahan dokumen persyaratan, seleksi hingga batas kuota daya tampung, pengumuman hasil seleksi secara terbuka, hingga daftar ulang.PPDB dilaksanakan secara objektif, transparan,dilaksanakan pada setiap tahun, dimulai pada awal bulan Juni.</p>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="<?= base_url('assets/img/berita/info.png'); ?>" class="d-block w-100 img-fluid" alt="Top Story Image 3">
+                    <h4 class="mt-3 fw-bold">Pengumuman Hasil Seleksi & Daftar ulang Tahun 2024/2025</h4>
+                    <p>Pengumuman Hasil Seleksi Tahap 1 dan Tahap 2 SMK N 2 Jayapura tahun ajaran 2024/2025 telah diumumkan silahkan cek melalui portal resmi sekolah kami.</p>
+                  </div>
+                </div>
+              </div>
             </article>
           </div>
 
-          <div class="col-12 col-md-6 mb-5">
-            <article>
-              <img src="<?= base_url('assets/img/berita/sains.jpg'); ?>" class="img-fluid same-size" alt="Bottom Left Image 1">
-              <h4 class="mt-3 fw-bold">Kegiatan Lomba Sains Tingkat Provinsi</h4>
-              <p>Siswa SMK N 2 Jayapura berhasil meraih juara 1 dalam lomba sains tingkat provinsi yang diadakan di Kota Jayapura.</p>
-              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
-            </article>
-          </div>
-
-          <div class="col-12 col-md-6 mb-5">
-            <article>
-              <img src="<?= base_url('assets/img/berita/b-2.jpg'); ?>" class="img-fluid same-size" alt="Bottom Left Image 2">
-              <h4 class="mt-3 fw-bold">Workshop Teknologi Informasi untuk Guru</h4>
-              <p>Guru-guru SMK N 2 Jayapura mengikuti workshop tentang teknologi informasi untuk meningkatkan keterampilan dalam pengajaran.</p>
-              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
-            </article>
-          </div>
-
-          <div class="col-12 col-md-6 mb-5">
-            <article>
-              <img src="<?= base_url('assets/img/berita/sains.jpg'); ?>" class="img-fluid same-size" alt="Bottom Left Image 1">
-              <h4 class="mt-3 fw-bold">Kegiatan Lomba Sains Tingkat Provinsi</h4>
-              <p>Siswa SMK N 2 Jayapura berhasil meraih juara 1 dalam lomba sains tingkat provinsi yang diadakan di Kota Jayapura.</p>
-              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
-            </article>
-          </div>
-
-          <div class="col-12 col-md-6 mb-5">
+          <div class="col-12 col-md-6 mb-4">
             <article>
               <img src="<?= base_url('assets/img/berita/b-2.jpg'); ?>" class="img-fluid same-size" alt="Bottom Left Image 2">
               <h4 class="mt-3 fw-bold">Workshop Teknologi Informasi untuk Guru</h4>
               <p>Guru-guru SMK N 2 Jayapura mengikuti workshop tentang teknologi informasi untuk meningkatkan keterampilan dalam pengajaran.</p>
-              <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
+              <!-- <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a> -->
+            </article>
+          </div>
+
+          <div class="col-12 col-md-6 mb-4">
+            <article>
+              <img src="<?= base_url('assets/img/berita/sains.jpg'); ?>" class="img-fluid same-size" alt="Bottom Left Image 1">
+              <h4 class="mt-3 fw-bold">Kegiatan Lomba Sains Tingkat Provinsi</h4>
+              <p>Siswa SMK N 2 Jayapura berhasil meraih juara 1 dalam lomba sains tingkat provinsi yang diadakan di Kota Jayapura.</p>
+              <!-- <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a> -->
             </article>
           </div>
         </div>
@@ -60,34 +58,19 @@
 
       <div class="col-lg-6">
         <h3 class="fw-bold mb-5 text-info">Berita Terbaru</h3>
-        <article class="d-flex mb-4">
-          <div class="me-3">
-            <h4 class="fw-bold">Pembagian Rapor Semester Ganjil 2024/2025</h4>
-            <p class="text-small">Pembagian rapor semester ganjil akan dilaksanakan pada tanggal 20 Desember 2024 di ruang kelas masing-masing.</p>
-            <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
-          </div>
-          <img src="<?= base_url('assets/img/berita/b-5.jpg'); ?>" class="img-fluid" alt="Right Image 1" style="width: 120px; height: auto;">
-        </article>
-        <hr class="border border-info border-1 opacity-50">
-        <article class="d-flex mb-4">
-          <div class="me-3">
-            <h4 class="fw-bold">Persiapan Ujian Praktik Kejuruan 2024</h4>
-            <p class="text-small">Ujian praktik kejuruan bagi kelas XII akan dilaksanakan mulai Januari 2024. Pastikan semua perlengkapan dan materi ujian telah disiapkan.</p>
-            <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
-          </div>
-          <img src="<?= base_url('assets/img/berita/b-5.jpg'); ?>" class="img-fluid" alt="Right Image 2" style="width: 120px; height: auto;">
-        </article>
-        <hr class="border border-info border-1 opacity-50">
-        <article class="d-flex mb-4">
-          <div class="me-3">
-            <h4 class="fw-bold">Kegiatan Perkemahan Pramuka di SMK N 2 Jayapura</h4>
-            <p class="text-small">Pramuka SMK N 2 Jayapura akan mengadakan kegiatan perkemahan tahunan pada bulan Oktober 2024. Kegiatan ini diikuti oleh seluruh siswa kelas X dan XI.</p>
-            <a href="#" class="text-decoration-none">Baca Selengkapnya <i class="fas fa-arrow-right"></i></a>
-          </div>
-          <img src="<?= base_url('assets/img/berita/b-5.jpg'); ?>" class="img-fluid" alt="Right Image 3" style="width: 120px; height: auto;">
-        </article>
-        <hr class="border border-info border-1 opacity-50">
+        <?php foreach ($berita as $b) : ?>
+          <article class="d-flex mb-4">
+            <div class="me-3">
+              <h4 class="fw-bold"><?= esc($b['judul']); ?></h4>
+              <p class="text-small"><?= esc($b['desk']); ?></p>
+              <p class="text-small"><span>Di buat Tanggal : </span><?= date('d M Y', strtotime($b['tgl'])); ?></p>
+            </div>
+            <img src="<?= base_url('assets/img/berita/' . $b['foto']); ?>" class="img-fluid" alt="Right Image 1" style="width: 120px; height: auto;">
+          </article>
+          <hr class="border border-info border-1 opacity-50">
+        <?php endforeach; ?>
       </div>
+    </div>
     </div>
   </section>
 
