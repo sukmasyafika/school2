ca4b4b0fce60896c7739d2f8560442ef9ab450ac
sukmasyafika <98916879+sukmasyafika@users.noreply.github.com>
2024-11-01 08:07:40 +0900

    Update BaseController.php

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var list<string>
      */
-    protected $helpers = ['auth'];
+    protected $helpers = ['auth', 'url'];
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
@@ -54,5 +54,7 @@ abstract class BaseController extends Controller
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
+
+        session();
     }
 }
