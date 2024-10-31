167d5038fe1a4ac8aa136af9c8ac69c67cced2cd
sukmasyafika <98916879+sukmasyafika@users.noreply.github.com>
2024-11-01 08:07:03 +0900

    Update navbar.php

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,33 +1,33 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-lg">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-lg" id="mainNav">
   <div class="container">
     <a class="navbar-brand" href="#">
       <img src="<?= base_url('assets/img/logo/1.png'); ?>" alt="Bootstrap" width="70">
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+      aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav m-auto fs-5 ">
-        <li class="nav-item mx-3 ">
-          <a class="nav-link" href="/">Home</a>
+        <li class="nav-item mx-3">
+          <a class="nav-link <?= (current_url() == base_url('/')) ? 'active' : '' ?>" href="/">Home</a>
         </li>
         <li class="nav-item mx-3">
-          <a class="nav-link" href="/pages/profil">Profil</a>
+          <a class="nav-link <?= (current_url() == base_url('pages/profil')) ? 'active' : '' ?>" href="/pages/profil">Profil</a>
         </li>
         <li class="nav-item mx-3">
-          <a class="nav-link" href="/pages/galeri">Galeri</a>
+          <a class="nav-link <?= (current_url() == base_url('pages/galeri')) ? 'active' : '' ?>" href="/pages/galeri">Galeri</a>
         </li>
         <li class="nav-item mx-3">
-          <a class="nav-link" href="/pages/berita">Berita</a>
+          <a class="nav-link <?= (current_url() == base_url('pages/berita')) ? 'active' : '' ?>" href="/pages/berita">Berita</a>
         </li>
         <li class="nav-item mx-3">
-          <a class="nav-link" href="/pages/contact">Contact</a>
+          <a class="nav-link <?= (current_url() == base_url('pages/contact')) ? 'active' : '' ?>" href="/pages/contact">Contact</a>
         </li>
       </ul>
       <ul class="navbar-nav mr-auto fs-5 shadow-sm">
         <li class="nav-item fw-bold btn btn-sm btn-outline-info">
-          <a class="nav-link" href="/pages/login">Login</a>
+          <a class="nav-link" href="<?= base_url('login'); ?>">Login</a>
         </li>
       </ul>
     </div>
